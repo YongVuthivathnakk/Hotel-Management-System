8ced911006be20aa1b114762e0ba4d4d6bf9481d
puthearithkeng <sunnypretty360@gmail.com>
2025-01-23 14:09:16 +0700

    update user

---
+++ User.java
@@ -0,0 +1,18 @@
+public class User {
+    // attribute for user 
+    String frist_name;
+    String last_name;
+    String email;
+    String phone_number;
+    String room_type;
+    int room_number;
+
+    //method for user
+    static void booking(){}
+    static void refundpolicy(){}
+    static void extra(){}
+    static void payment(){}
+    static void receipt(){}
+    static void checkin(){}
+    static void checkout(){}
+}
