3533da5e53c7a3a19aabe8eb944e693fcaec9cb5
Gek Heang <muonggekheang05@gmail.com>
2025-01-23 14:42:52 +0700

    finish payment class, working on booking class

---
+++ Booking.java
@@ -0,0 +1,15 @@
+class Booking {
+    // attribute
+    String userName;
+    String bookingId;
+    String date;
+    int numberOfRoom;
+    int numberOfAdultGuest;
+    int numberOfKidGuest;
+    int discount;
+    double price;
+    String phoneNumber;
+
+    // constructor
+    
+}
---
+++ Payment.java
@@ -0,0 +1,18 @@
+class Payment{
+    // attribute
+    String cardNumber;
+   
+    String status;
+
+    // constructor 1 pay by cash
+    Payment(){
+        status = "Pending";
+    }
+    
+    // constructor 2 pay by card 
+    Payment(String inputCardNumber){
+        cardNumber = inputCardNumber;
+        status = "Paid";
+    }
+
+}
