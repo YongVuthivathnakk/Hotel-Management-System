4b482620f1ca2147149fcb82bf5d98f210309f9e
puthearithkeng <sunnypretty360@gmail.com>
2025-01-24 15:09:30 +0700

    delete

--- User.java
+++ User.java
@@ -1,46 +1,15 @@
 public class User {
+    int totalUser = 0;
     // attribute for user 
-    String frist_name;
-    String last_name;
     String email;
     String password ;
-    String comfirm_password;
-    String phone_number;
+
     // for signin acc
     public User (String signin_email , String singin_password ){
         email = signin_email;
         password = singin_password ;
+        totalUser += 1;
     }
-    // for signup acc
-    public User (String signin_email , String singin_password  , String comfirm_password){
-        if (password != comfirm_password) {
-            System.err.println("Passwords do not match!");
-        }
-        email = signin_email;
-        password = singin_password ;
-    }
-
-    //method for user
-    static void booking(){ // for user booking
-
-    }
-    static void refundpolicy(){ // for refund 
-
-    }
-    static void extra(){ // for extra room , food , other
-
-    }
-    static void payment(){ // for payment using card or money
-
-    }
-    static void receipt(){ // for receipt when already booking using form
-
-    }
-    static boolean checkin(){ // to checkin when go in
-        return true ;
-    }
-    static boolean checkout(){ // to checout when go out
-        return true ;
-    }
+    
 
 }
