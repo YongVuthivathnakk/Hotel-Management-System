a851741793de1df4922e1720bae5da08192d37d9
Thyda <sothyda.mao@gmail.com>
2025-01-24 15:06:01 +0700

    changes

--- Extra.java
+++ Extra.java
@@ -1,8 +1,9 @@
 public class Extra {
     private String name;
     private float price;
-}
-public Extra(String name,float price){
+
+public Extra(String name, float price){
     this.name = name;
     this.price = price;
+}
 }
--- Room.java
+++ Room.java
@@ -1,13 +1,18 @@
 public class Room {
     private int roomNumber;
+    private String description;
+    private int capacity;
     private String roomType;
     private float  pricePerNight;
     private boolean isAvailable;
 
-    public Room(int roomNumber, String roomType, float pricePerNight) {
+
+    public Room(int roomNumber, String roomType, float pricePerNight , String description, int capacity) {
         this.roomNumber = roomNumber;
         this.roomType = roomType;
         this.pricePerNight = pricePerNight;
+        this.capacity = capacity;
+        this.description = description;
         this.isAvailable = true;
     }
 }
