5db8ca2a14ad8ad850ed17ddb18a803ff84aa863
puthearithkeng <sunnypretty360@gmail.com>
2025-01-24 14:08:01 +0700

    delete form

--- User.java
+++ User.java
@@ -6,22 +6,6 @@ public class User {
     String password ;
     String comfirm_password;
     String phone_number;
-    String room_type;
-    int room_number;
-    int arrival_date ;
-    int departure_date;
-
-    // constructor for infor form
-    public User(String newfirst_name, String newlast_name, String newemail, String newphone_number, String newroom_type, int newroom_number , int newarrival_date , int newdeparture_date) {
-        frist_name = newfirst_name;
-        last_name = newlast_name;
-        email = newemail;
-        phone_number = newphone_number;
-        room_type = newroom_type;
-        room_number = newroom_number;
-        arrival_date = newarrival_date;
-        departure_date =newdeparture_date;
-    }
     // for signin acc
     public User (String signin_email , String singin_password ){
         email = signin_email;
