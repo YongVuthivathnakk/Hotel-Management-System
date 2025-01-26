ee50a63e4adcd5ee52c9b6249fc8a1e19158b4a1
puthearithkeng <sunnypretty360@gmail.com>
2025-01-26 23:19:09 +0700

    new construtor

---
+++ guest.java
@@ -0,0 +1,17 @@
+public class guest {
+    //personal infor of guest
+    String fullName;
+    String phone;
+    String email;
+    String address; 
+    String nationality;
+
+    //contractor
+    void PersonalInformation(String fullName, String phone, String email, String address, String nationality) {
+        this.fullName = fullName;
+        this.phone = phone;
+        this.email = email;
+        this.address = address;
+        this.nationality = nationality;
+    }
+}
