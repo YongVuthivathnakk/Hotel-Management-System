d91735267cb69ee240ce2787ae61831c3f08bb6b
puthearithkeng <sunnypretty360@gmail.com>
2025-01-29 22:59:34 +0700

    take out public

--- requestForCheckIn.java
+++ requestForCheckIn.java
@@ -3,7 +3,7 @@ public class requestForCheckIn {
     String checkinPhone;
     String checkinGmail;
 
-    public requestForCheckIn(String checkinFullName, String checkinPhone, String checkinGmail) {
+    requestForCheckIn(String checkinFullName, String checkinPhone, String checkinGmail) {
         this.checkinFullName = checkinFullName;
         this.checkinPhone = checkinPhone;
         this.checkinGmail = checkinGmail;
--- requestForCheckOut.java
+++ requestForCheckOut.java
@@ -3,7 +3,7 @@ public class requestForCheckOut {
     String checkoutPhone;
     String checkoutGmail;
 
-    public requestForCheckOut(String checkoutFullName, String checkoutPhone, String checkoutGmail) {
+    requestForCheckOut(String checkoutFullName, String checkoutPhone, String checkoutGmail) {
         this.checkoutFullName = checkoutFullName;
         this.checkoutPhone = checkoutPhone;
         this.checkoutGmail = checkoutGmail;
