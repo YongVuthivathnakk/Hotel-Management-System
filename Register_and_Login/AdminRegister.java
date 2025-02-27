93f5a884bc55a751f8ba1987547672d0f7c87df7
Nakk47 <pozz.vathhd@gmail.com>
2025-02-28 00:08:09 +0700

    add register for admin

--- Register_and_Login/AdminRegister.java
+++ Register_and_Login/AdminRegister.java
@@ -1,49 +1,46 @@
 package Register_and_Login;
-
+import Register_and_Login.Validator;
+import java.util.InputMismatchException;
 import java.util.Scanner;
-import java.util.regex.Pattern;
+
+import Booking.bookingTesting;
 import User.Admin;
 
-
     public class AdminRegister {
-        public static boolean isValid(String email) {
-    
-        // Regular expression to match valid email formats
-        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@" +
-                            "(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
-    
-        // Compile the regex
-        Pattern p = Pattern.compile(emailRegex);
-        
-        // Check if email matches the pattern
-        return email != null && p.matcher(email).matches();
-    }
-
 
 
     // Main
     public static void main(String[] args) {
+
+// =================== Variable ========================
         Scanner input = new Scanner(System.in);
         String firstName;
         String lastName;
         String userName;
-        String email;
-        String status;
-        String password;
         String gender;
+        int age;
+        String email;
+        String phoneNumber;
+        String status;
+        int tempStatus;
+        String password;
+        double salary;
+        Validator validate = new Validator();
+
+// ======================================================
 
         System.out.println("==== Admin Registration =====\n");
         //Enter the fist name
         System.out.print("Enter your First name: ");
-        firstName = input.nextLine().replaceAll(" ", ""); // Remove all empty space
+        firstName = input.nextLine().trim().replaceAll(" ", ""); // Remove all empty space
 
         // Enter the Last Name
         System.out.print("Enter your Last name: ");
-        lastName = input.nextLine().replaceAll(" ", ""); // Remove all empyt space
+        lastName = input.nextLine().trim().replaceAll(" ", ""); // Remove all empyt space
         
         // Enter the user name
         System.out.print("Enter your User name: ");
-        userName = input.nextLine().replaceAll(" ", "");
+        userName = input.nextLine().trim().replaceAll(" ", "");
         
         // Enter gender
         
@@ -58,23 +55,93 @@ import User.Admin;
         }
 
         while (true) {
-            System.out.println("Enter your email address: ");
-            email = input.nextLine();
-            if (isValid(email)) {
+            try {
+                System.out.print("Enter your age: ");
+                age = input.nextInt();
+                if (age > 18 && age <= 60) {
+                    input.nextLine(); // clear buffer
+                    break;
+                } else {
+                    System.out.println("Age must be 18 - 60 !!!");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Please enter interger!");
+                input.nextLine(); // clear buffer
+            }
+        }
+
+        while (true) {
+            System.out.print("Enter your email address: ");
+            email = input.nextLine().replaceAll(" ", "");
+            if (validate.isEmailValid(email)) {
                 break;
             } else {
                 System.out.println("Wrong email address format!!");
             }
         }
-        
-        
 
+        while (true) {
+            System.out.print("Enter your phone number: ");
+            phoneNumber = input.nextLine().trim().replaceAll(" ", "");
+            if (validate.isPhoneNumberValid(phoneNumber)) {
+                break;
+            } else {
+                System.out.println("Your number must start with [7-9] and followed by [0-9] of 9 digits.");
+            }
+        }
+        
+        while (true) {
+            try {
+                System.out.println("Enter your Status (1 = Online, 0 = Offline): ");
+                tempStatus = input.nextInt();
+                if (tempStatus == 1) {
+                    status = "Online";
+                    input.nextLine(); // clear buffer
+                    break;
+                } else if (tempStatus == 0){
+                    status = "Offline";
+                    input.nextLine(); // clear buffer
+                    break;
+                } else {
+                    System.out.println("Sorry, wrong input !!!");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Please enter interger!");
+                input.nextLine(); // clear buffer
+            }
+        }
+
+        while (true) {
+            try {
+                System.out.print("Enter your salary (must be equal to or more than 100): ");
+                salary = input.nextDouble();
+                if (salary >= 100) {
+                    input.nextLine();
+                    break;
+                }
+                break;
+            } catch (InputMismatchException e) {
+               System.out.println("Please enter either interger or double number!");
+               input.nextLine();
+            }
+        }
+
+        while (true) {
+            System.out.print("Enter your Password: ");
+            password = input.nextLine();
+            if (password.length() >= 8) {
+                break;
+            } else {
+                System.out.println("Your password is too short !!!");
+            }
+        }
+        
+        // create admin
+        Admin newAdmin = new Admin(firstName, lastName, userName, gender, age, email, phoneNumber, password, status, salary);
+        System.out.print(newAdmin.toString());
 
         input.close();
 
-        // System.out.println("Enter your phone number: ");
-        // System.out.println("Enter your Status: ");
-        // System.out.println("Enter your Password: ");
 
     }
 }
---
+++ User/User.java
@@ -0,0 +1,59 @@
+package User;
+
+import java.util.regex.Pattern;
+
+public abstract class User {
+    protected String firstName;
+    protected String lastName;
+    protected String email;
+    protected String phoneNumber;
+    protected String userName;
+    protected String password;
+    protected String gender;
+    protected int age;
+
+
+    public User(String firstName, String lastName, String userName, String gender, int age ,String email, String phoneNumber, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.gender = gender;
+        this.age = age;
+        this.phoneNumber = phoneNumber;
+        this.password = password;
+        this.userName = userName;
+    }
+
+    // Getters
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public String getPhoneNumber() {
+        return this.phoneNumber;
+    }
+
+    public String getUserName() {
+        return this.userName;
+    }
+
+    public String getGender() {
+        return this.gender;
+    }
+
+    public int age() {
+        return this.age;
+    }
+
+
+    // method
+
+}
