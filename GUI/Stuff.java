cc5ae7d5191f261d276232b9c890c4e0a5fcffee
YongVuthivathnakk <vuthivathnakkyong@gmail.com>
2025-03-26 09:11:36 +0700

    resolve conflic

--- GUI/Stuff.java
+++ GUI/Stuff.java
@@ -6,21 +6,29 @@ import javax.swing.JPanel;
 
 
 public class Stuff {
-    Frame myFrame = new Frame();
-    JPanel panel = new JPanel();
-    Button assignRoomButton = new Button("Assign New Room", 50, 100);
-    Button readRoomButton = new Button("Load Room data", 250, 100);
-    Button bookingButton = new Button("Booking", 120, 160);
-    Button paymentButton = new Button("Payment", 120, 220);
-
-    public Stuff(){
-
-        panel.setLayout(null);
-        panel.setBackground(Color.DARK_GRAY);
-        panel.setBounds(200,250,400,300);
+    public static void main(String[] args) {
+        Frame myFrame = new Frame();
+        JPanel categoryPanel = new JPanel();
+        JPanel rightPanel = new JPanel();
+        categoryPanel.setLayout(null);
+        categoryPanel.setBackground(Color.DARK_GRAY);
+        categoryPanel.setBounds(0,0,300,800);
+        rightPanel.setLayout(null);
+        rightPanel.setBackground(Color.white);
+        rightPanel.setBounds(300,0,900,800);
         myFrame.getContentPane().setBackground(Color.darkGray);
-
-
+        Button assignRoomButton = new Button("Assign New Room", 50, 100);
+        Button readRoomButton = new Button("Load Room data", 50, 160);
+        Button updateRoomButton = new Button("Update Room info",50, 220);
+        Button bookingButton = new Button("Booking", 50, 280);
+        Button paymentButton = new Button("Payment", 50, 340);
+        categoryPanel.add(assignRoomButton);
+        categoryPanel.add(readRoomButton);
+        categoryPanel.add(bookingButton);
+        categoryPanel.add(paymentButton);
+        categoryPanel.add(updateRoomButton);
+        myFrame.add(categoryPanel);
+        myFrame.add(rightPanel);
 
 
         assignRoomButton.addActionListener(e ->{
@@ -29,14 +37,14 @@ public class Stuff {
                 AssignNewRoom assignNewRoom = new AssignNewRoom();
             }
         });
-    
+
         readRoomButton.addActionListener(e -> {
             if(e.getSource() == readRoomButton){
                 System.out.println("Load Room data button is clicked");
                 LoadRoomFromDatabase loadRoomData = new LoadRoomFromDatabase();
             }
         });
-    
+
         bookingButton.addActionListener(e -> {
             if(e.getSource() == bookingButton){
                 System.out.println("Booking button is clicked");
@@ -44,11 +52,11 @@ public class Stuff {
             }
         });
 
-        panel.add(assignRoomButton);
-        panel.add(readRoomButton);
-        panel.add(bookingButton);
-        panel.add(paymentButton);
-        myFrame.add(panel);
+        updateRoomButton.addActionListener(e ->{
+            if(e.getSource() == updateRoomButton){
+                System.out.println("Update button is clicked");
+                UpdateRoomInfo updateRoomInfo = new UpdateRoomInfo();
+            }
+        });
     }
-
 }
